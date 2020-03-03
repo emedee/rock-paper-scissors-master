648265f89c8c99f01d2f6eb3f6cad43c9e59e347
emedee <emediongini@gmail.com>
2020-03-02 12:31:19 -1200

    'commit'

--- Script.js
+++ Script.js
@@ -7,11 +7,16 @@ let scissors = document.querySelector('.scissors');
 let rockSelect = document.querySelector('.rock-select')
 let paperSelect = document.querySelector('.paper-select')
 let scissorsSelect = document.querySelector('.scissors-select')
+let rockSelectHouse = document.querySelector('.rock-select-house')
+let paperSelectHouse = document.querySelector('.paper-select-house')
+let scissorsSelectHouse = document.querySelector('.scissors-select-house')
 let mySelection = document.querySelector('.you-picked');
 let content = document.querySelector('.content')
 let content2 = document.querySelector('.content2');
 let result = document.querySelector('.show-result');
-let playAgain = document.querySelector('.btn-playAgain')
+let playAgain = document.querySelector('.btn-playAgain');
+let loseOrWin = document.querySelector('.selected-result');
+let randomSelect = [rockSelectHouse, paperSelectHouse, scissorsSelectHouse]
 
 btnRules.addEventListener('click', function(){
     rules.style.display = 'block';
@@ -26,6 +31,15 @@ rock.addEventListener('click', function(){
     rockSelect.style.display = 'block';
     paperSelect.style.display = 'none';
     scissorsSelect.style.display = 'none';
+    var test = randomSelect[Math.floor(Math.random() * randomSelect.length)]
+    test.style.display = 'block';
+    if(test == paperSelectHouse){
+        loseOrWin.innerHTML = 'YOU LOSE'
+    } else if (test == scissorsSelectHouse){
+        loseOrWin.innerHTML = 'YOU WIN'
+    } else {
+        loseOrWin.innerHTML = 'TIE'
+    }
     result.style.display = 'flex';
 })
 paper.addEventListener('click', function(){
@@ -34,6 +48,15 @@ paper.addEventListener('click', function(){
     rockSelect.style.display = 'none';
     paperSelect.style.display = 'block';
     scissorsSelect.style.display = 'none';
+    var test = randomSelect[Math.floor(Math.random() * randomSelect.length)]
+    test.style.display = 'block';
+    if(test == scissorsSelectHouse){
+        loseOrWin.innerHTML = 'YOU LOSE'
+    } else if (test == rockSelectHouse){
+        loseOrWin.innerHTML = 'YOU WIN'
+    } else {
+        loseOrWin.innerHTML = 'TIE'
+    }
     result.style.display = 'block';
 })
 scissors.addEventListener('click', function(){
@@ -42,9 +65,17 @@ scissors.addEventListener('click', function(){
     rockSelect.style.display = 'none';
     paperSelect.style.display = 'none';
     scissorsSelect.style.display = 'block';
+    var test = randomSelect[Math.floor(Math.random() * randomSelect.length)]
+    test.style.display = 'block';
+    if(test == rockSelectHouse){
+        loseOrWin.innerHTML = 'YOU LOSE'
+    } else if (test == paperSelectHouse){
+        loseOrWin.innerHTML = 'YOU WIN'
+    } else {
+        loseOrWin.innerHTML = 'TIE'
+    }
     result.style.display = 'flex';
 })
 playAgain.addEventListener('click', function(){
-    content.style.display = 'flex';
-    content2.style.display = 'none';
+    location.reload();
 })
